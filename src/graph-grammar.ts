5984dce65c9aa95dd0d498ce99a0bee0cf57f075
DeLucaJ <18635544+DeLucaJ@users.noreply.github.com>
2019-10-23 13:43:57 -0700

    Adds 'step' and 'replaceSubGraph' functions to graph-grammar.ts

--- src/graph-grammar.ts
+++ src/graph-grammar.ts
@@ -17,13 +17,86 @@ export interface Letter {
 }
 
 export const narrativeGrammar: GraphGrammar = {
-    alphabet: [],
+    alphabet: [
+        { name: "S", terminal: false },
+        { name: "C", terminal: false },
+        { name: "CL", terminal: false },
+        { name: "CP", terminal: false },
+        { name: "F", terminal: false },
+        { name: "H", terminal: false },
+        { name: "m", terminal: true },
+        { name: "p", terminal: true },
+        { name: "bp", terminal: true },
+        { name: "mp", terminal: true },
+        { name: "ii", terminal: true },
+        { name: "c", terminal: true },
+        { name: "r", terminal: true },
+        { name: "and", terminal: true },
+        { name: "or", terminal: true },
+        { name: "br", terminal: true }
+    ],
     rules: []
 }
-
+ 
 export const questGrammar: GraphGrammar = {
-    alphabet: [],
-    rules: []
+    alphabet: [
+        { name: 'S', terminal: false }, // 0
+        { name: 'C', terminal: false }, // 1
+        { name: 'CF', terminal: false },// 2
+        { name: 'CL', terminal: false },// 3
+        { name: 'CP', terminal: false },// 4
+        { name: 'F', terminal: false }, // 5
+        { name: 'G', terminal: false }, // 6
+        { name: 'H', terminal: false }, // 7
+        { name: 'bl', terminal: true }, // 8
+        { name: 'bm', terminal: true }, // 9
+        { name: 'e', terminal: true },  // 10
+        { name: 'g', terminal: true },  // 11
+        { name: 'ib', terminal: true }, // 12
+        { name: 'iq', terminal: true }, // 13
+        { name: 'k', terminal: true },  // 14
+        { name: 'kf', terminal: true }, // 15
+        { name: 'km', terminal: true }, // 16
+        { name: 'l', terminal: true },  // 17
+        { name: 'lf', terminal: true }, // 18
+        { name: 'lm', terminal: true }, // 19
+        { name: 'n', terminal: true },  // 20
+        { name: 'ec', terminal: true }, // 21
+        { name: 'ei', terminal: true }, // 22
+        { name: 'es', terminal: true }  // 23
+    ],
+    rules: [
+        { // (S) => (e -> C -> G -> bm -> iq -> ei -> CF -> g)
+            input: {
+                vertices: [{ label: 0 }],
+                edges: [],
+            },
+            output: {
+                // each index directly corresponds, and will be replaced
+                // leave any nodes that need to stay the same at the same index. 
+                vertices: [
+                    { label: 10 },  // 0
+                    { label: 1 },   // 1
+                    { label: 6 },   // 2
+                    { label: 9 },   // 3
+                    { label: 13 },  // 4
+                    { label: 22 },  // 5
+                    { label: 2 },   // 6
+                    { label: 11 }   // 7
+                ],
+                edges: [
+                    { source: 0, target: 1 },
+                    { source: 1, target: 2 },
+                    { source: 2, target: 3 },
+                    { source: 3, target: 4 },
+                    { source: 4, target: 5 },
+                    { source: 5, target: 6 },
+                    { source: 6, target: 7 }
+                ]
+            },
+            terminator: false
+        }
+    ]
 }
 
 export const structureGrammar: GraphGrammar = {
@@ -52,29 +125,13 @@ export function interpret(graph: Graph.Graph, grammar: GraphGrammar, steps: numb
  * Runs a single, random, and valid rule from the grammar
  */
 function step(graph: Graph.Graph, grammar: GraphGrammar) { 
-    const rsg: [Rule, Graph.Graph] = findValidRule(graph, grammar);
-    replaceSubGraph(graph, grammar, rsg[0], rsg[1]);
-}
-
-/*
- * @param graph: the Graph we want to use to validate rules 
- * @param grammar: the Grammar we want to draw rules and alphabet from
- * @returns: the index of the chosen rule in the grammar.rules array
- * 
- * This function finds a rule that can be run on the subgraph
- */ 
-function findValidRule(graph: Graph.Graph, grammar: GraphGrammar): [Rule, Graph.Graph] { 
-    return [grammar.rules[-1], graph]
-}
-
-/*
- * @param graph: the Graph we want to grow 
- * @param grammar: the Grammar we want to draw rules and alphabet from
- * @param rule: the rule from which we want to draw the target graph and replacement graph from
- * 
- * This function swaps a subgraph of graph matching the input of the rule with the output in the rule
- */
-function replaceSubGraph(graph: Graph.Graph, grammar: GraphGrammar, rule: Rule, subgraph: Graph.Graph) { 
+    const validRules: [Rule, Graph.Converter][] = [];
+    for (let rule of grammar.rules) {
+        let converter: Graph.Converter = matchSubGraph(graph, rule.input);
+        if (converter.valid) validRules.push([rule, converter]);
+    }
+    const index = Math.floor(Math.random() * (validRules.length - 1));
+    replaceSubGraph(graph, validRules[index][0].output, validRules[index][1]);
 }
 
 /*
@@ -84,8 +141,47 @@ function replaceSubGraph(graph: Graph.Graph, grammar: GraphGrammar, rule: Rule, 
  * 
  * This function finds a valid subgraph in the starting graph in order to replace
  */
-function findSubGraph(graph: Graph.Graph, grammar: GraphGrammar, rule: Rule): Graph.Graph { 
-    return graph; 
+function matchSubGraph(graph: Graph.Graph, subgraph: Graph.Graph): Graph.Converter { 
+    const vertexMap: Map<number, number> = new Map();
+    const edgeMap: Map<number, number> = new Map();
+
+    // Subgraph Isomorphism Problem
+
+    return new Graph.Converter(vertexMap, edgeMap, false);
+}
+
+/*
+ * @param graph: the Graph we want to grow 
+ * @param grammar: the Grammar we want to draw rules and alphabet from
+ * @param rule: the rule from which we want to draw the target graph and replacement graph from
+ * 
+ * This function swaps a subgraph of graph matching the input of the rule with the output in the rule
+ */
+function replaceSubGraph(graph: Graph.Graph, newsub: Graph.Graph, converter: Graph.Converter) {
+    for (let i = 0; i < newsub.vertices.length; i++) {
+        const current: Graph.Vertex = newsub.vertices[i];
+        if (converter.vertexMap.has(i)) {
+            Graph.replaceVertex(graph, converter.vertexMap.get(i) as number, current);
+        }
+        else {
+            Graph.addVertex(graph, current);
+            converter.vertexMap.set(i, graph.vertices.length - 1);
+        }
+    }
+    // all vertecies should have an entry in the map now
+    for (let i = 0; i < newsub.edges.length; i++) {
+        const current: Graph.Edge = newsub.edges[i];
+        const newEdge: Graph.Edge = { 
+            source: converter.vertexMap.get(current.source) as number, 
+            target: converter.vertexMap.get(current.target) as number
+        };
+        if (converter.edgeMap.has(i)) {
+            Graph.replaceEdge(graph, converter.edgeMap.get(i) as number, newEdge);
+        }
+        else {
+            Graph.addEdge(graph, newEdge);
+        }
+    }
 }
 
 /*
--- src/graph.ts
+++ src/graph.ts
@@ -5,13 +5,23 @@ export interface Graph {
 
 export interface Vertex {
     label: number;
-    parents: number[];
-    children: number[];
 }
 
 export interface Edge {
     source: number;
     target: number;
+}
+
+export class Converter {
+    valid: boolean = false;
+    vertexMap: Map<number, number>;
+    edgeMap: Map<number, number>;
+
+    constructor(vertexMap: Map<number, number>, edgeMap: Map<number, number>, valid?: boolean) {
+        this.vertexMap = vertexMap;
+        this.edgeMap = edgeMap;
+        if (valid) this.valid = valid;
+    }
 }
 
 export function addVertex(graph: Graph, vertex: Vertex) {
@@ -29,3 +39,26 @@ export function removeVertex(graph: Graph, vertex: Vertex) {
 export function removeEdge(graph: Graph, edge: Edge) {
     graph.edges.splice(graph.edges.indexOf(edge), 1);
 }
+
+export function replaceEdge(graph: Graph, index: number, edge: Edge) {
+    graph.edges.splice(index, 1, edge)
+}
+
+export function replaceVertex(graph: Graph, index: number, vertex: Vertex) {
+    graph.vertices.splice(index, 1, vertex);
+}
+
+// Returns all edges in the graph that have the same source and target
+export function findEdges(graph: Graph, s: number, t: number): Edge[] {
+    const edges: Edge[] = [];
+    graph.edges.forEach(function (edge: Edge) {
+        let smatch = (s == -1) ? true : false;
+        let tmatch = (t == -1) ? true : false;
+        if (s > -1 && edge.source == s) smatch = true;
+        if (t > -1 && edge.target == t) tmatch = true;
+        if (smatch && tmatch) edges.push(edge);
+    });
+    return edges;
+}
+
+
