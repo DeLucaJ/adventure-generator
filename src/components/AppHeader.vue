932f400fbafdfb4c765767792340561994f4b6a3
DeLucaJ <delucaj96@gmail.com>
2019-10-30 13:46:35 -0700

    Adds proper Classing to components, adds AppHeader and AppFooter components to App

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,6 +10,13 @@
         </h2>
       </div>
     </div>
+    <div class="hero-foot">
+      <!-- Needs to be done with proper Buefy Navbar -->
+      <div class="container" id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
   </section>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,3 @@
     <h1>This is the Home Page</h1>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-
-export default {
-  name: 'home',
-  components: {
-    
-  }
-}
-</script>
