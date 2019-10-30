a79e3c8137b63f9b1c6aceb4331fe531e2594171
DeLucaJ <delucaj96@gmail.com>
2019-10-30 13:53:30 -0700

    Adds working Navbar to AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,8 +13,14 @@
     <div class="hero-foot">
       <!-- Needs to be done with proper Buefy Navbar -->
       <div class="container" id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <!-- <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> -->
+        <b-navbar>
+          <template slot='start'>
+            <b-navbar-item tag="router-link" :to="{ path: '/'}">Home</b-navbar-item>
+            <b-navbar-item tag="router-link" :to="{ path: '/about'}">About</b-navbar-item>
+          </template>
+        </b-navbar>
       </div>
     </div>
   </section>
