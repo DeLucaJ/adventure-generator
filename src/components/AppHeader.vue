f392bc5c19fa89f3b4e85673bcb3dfcd7aedaae1
DeLucaJ <delucaj96@gmail.com>
2019-10-30 13:55:56 -0700

    Removes extraneous comments from AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,10 +11,7 @@
       </div>
     </div>
     <div class="hero-foot">
-      <!-- Needs to be done with proper Buefy Navbar -->
       <div class="container" id="nav">
-        <!-- <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> -->
         <b-navbar>
           <template slot='start'>
             <b-navbar-item tag="router-link" :to="{ path: '/'}">Home</b-navbar-item>
