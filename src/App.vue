0781a56b51c78fbc854911cc9b333a35dae14446
DeLucaJ <delucaj96@gmail.com>
2019-11-06 10:11:57 -0800

    Fixes issue on About Page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <!-- <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css"> -->
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
     <AppHeader/>
     <router-view/>
     <AppFooter style="bottom: 0"/>
