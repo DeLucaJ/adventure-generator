cef81034d8ad31ec6d4edd022431ade601926e06
DeLucaJ <18635544+DeLucaJ@users.noreply.github.com>
2019-11-06 22:40:47 -0800

    Changes text on about page to a subtitle

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container page has-text-centered">
     <h1 class="title is-1">Welcome to Adventure Generator</h1>
-    <p>A tool to help Game Masters craft and run adventures</p>
+    <p class="subtitle">A tool to help Game Masters craft and run adventures</p>
     <br/>
     <div>
       <b-button size="is-large" icon-left="play" class="is-primary">Get Started</b-button>
