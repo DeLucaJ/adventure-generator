70a2463ec6a1b3639b944be3c28005aa2c7c87e1
DeLucaJ <delucaj96@gmail.com>
2019-11-13 15:10:15 -0800

    Renames VillainBlock, adds FactionBlock

--- src/components/viewer-blocks/AdversaryBlock.vue
+++ src/components/viewer-blocks/AdversaryBlock.vue
@@ -6,7 +6,7 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class VillainBlock extends Vue {
+export default class AdversaryBlock extends Vue {
 
 }
 </script>
---
+++ src/components/viewer-blocks/FactionBlock.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FactionBlock extends Vue {
+
+}
+</script>
