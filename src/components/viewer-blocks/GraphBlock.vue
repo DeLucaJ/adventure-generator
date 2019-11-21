d6caca3e3422105db0c0b0ba09da3259faa7bde9
DeLucaJ <delucaj96@gmail.com>
2019-11-20 22:37:47 -0800

    Swaps Cytoscape for GoJS

--- src/components/viewer-blocks/GraphBlock.vue
+++ src/components/viewer-blocks/GraphBlock.vue
@@ -1,9 +1,7 @@
 <template>
   <section class="section">
     <div class="container">
-      <h4 class="title is-4">Graph: </h4>
-      <!-- Graph -->
-      <div id="cy"/>
+      
     </div>
   </section>
 </template>
@@ -11,36 +9,18 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import Graph from "@/generators/graph";
-// import * as CS from "cytoscape";
-import cytoscape from "cytoscape";
 
 @Component
 export default class GraphBlock extends Vue {
   @Prop({ default: new Graph() })
   graph!: Graph;
 
-  instance!: cytoscape.Core;
-
   mounted() {
-    this.instance = cytoscape({
-      container: document.getElementById('cy'),
-      elements: this.graph.cytoscape(),
-      layout: {
-        name: 'breadthfirst',
-        fit: true
-      },
-      zoom: 1,
-      zoomingEnabled: false
-    });
-    console.log(this.instance);
+
   }
 }
 </script>
 
 <style lang="scss">
-#cy {
-  width: 100%;
-  height: 100%;
-  min-height: 400px;
-}
+
 </style>
--- src/generators/graph.ts
+++ src/generators/graph.ts
@@ -10,40 +10,6 @@ export default class Graph {
         this.edges = edges;
     }
 
-    cytoscape(): cytoscape.ElementsDefinition {
-        let vertMap: Map<string, number> = new Map();
-        const elements: cytoscape.ElementsDefinition = {
-            nodes: [],
-            edges: []
-        }
-        for (let vertex of this.vertices) {
-            let cyvert: cytoscape.NodeDefinition = {
-                data: { id: "" }
-            }
-            if (vertMap.has(vertex.label)) {
-                vertMap.set(vertex.label, (vertMap.get(vertex.label) as number) + 1);
-            }
-            else {
-                vertMap.set(vertex.label, 1);
-            }
-            cyvert.data.id = vertex.label + (vertMap.get(vertex.label) as number).toString();
-            cyvert.data.label = vertex.label;
-            elements.nodes.push(cyvert);
-        }
-        for (let edge of this.edges) {
-            let cyedge: cytoscape.EdgeDefinition = {
-                data: {
-                    id: (elements.edges.length + 1).toString() as string,
-                    source: elements.nodes[edge.source].data.id as string,
-                    target: elements.nodes[edge.target].data.id as string
-                }
-            }
-            elements.edges.push(cyedge);
-        }
-        
-        return elements;
-    }
-
     addVertex(vertex: Vertex) {
         this.vertices.push(vertex);
     }
