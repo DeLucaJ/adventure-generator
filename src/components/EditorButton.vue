6e71069174654e4a4ac6ed8f8973c1f5ced687a6
DeLucaJ <18635544+DeLucaJ@users.noreply.github.com>
2019-11-27 11:43:03 -0800

    Moved Editor Modal Functionality to the EditorButton component for less modal repetition

---
+++ src/components/EditorButton.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="editor-button">
+    <b-button class='is-text' icon-left="pencil" @click="edit()"></b-button>
+    <!-- EditorModal -->
+    <b-modal trap-focus has-modal-card :active.sync="modalActive">
+      <component :is="editor" :subject="element"/>
+    </b-modal>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import {
+  Element,
+  Adventure,
+  Narrative,
+  Quest,
+  Encounter,
+  PlotPoint,
+  Setting,
+  Character,
+  Area,
+  Item,
+  Faction
+} from "@/types";
+import {
+  AdventureEditor,
+  AreaEditor,
+  CharacterEditor,
+  EncounterEditor,
+  FactionEditor,
+  ItemEditor,
+  NarrativeEditor,
+  PlotPointEditor,
+  QuestEditor, 
+  SettingEditor
+} from "@/components/editors";
+
+@Component({
+  components: {
+    AdventureEditor,
+    AreaEditor,
+    CharacterEditor,
+    EncounterEditor,
+    FactionEditor,
+    ItemEditor,
+    NarrativeEditor,
+    PlotPointEditor,
+    QuestEditor, 
+    SettingEditor
+  }
+})
+export default class EditorButton extends Vue {
+  @Prop()
+  element!: Element;
+  editor: string = "";
+  modalActive: boolean = false;
+
+  mounted() {
+    if (this.element instanceof Adventure) {
+      this.editor = `AdventureEditor`;
+    }
+    else if (this.element instanceof Narrative) {
+      this.editor = `NarrativeEditor`;
+    }
+    else if (this.element instanceof Quest) {
+      this.editor = `QuestEditor`;
+    }
+    else if (this.element instanceof Encounter) {
+      this.editor = `EncounterEditor`;
+    }
+    else if (this.element instanceof PlotPoint) {
+      this.editor = `PlotPointEditor`;
+    }
+    else if (this.element instanceof Setting) {
+      this.editor = `SettingEditor`;
+    }
+    else if (this.element instanceof Area) {
+      this.editor = `AreaEditor`;
+    }
+    else if (this.element instanceof Character) {
+      this.editor = `CharacterEditor`;
+    }
+    else if (this.element instanceof Item) {
+      this.editor = `ItemEditor`;
+    }
+    else if (this.element instanceof Faction) {
+      this.editor = `FactionEditor`;
+    }
+  }
+
+  edit() {
+    this.modalActive = true;
+  }
+}
+
+</script>
--- src/components/ElementCard.vue
+++ src/components/ElementCard.vue
@@ -3,103 +3,26 @@
     <header class="card-header">
       <p class="card-header-title">{{ element.title }}</p>
       <div class="buttons is-pulled-right">
-        <b-button class='is-text' icon-left="pencil" @click="edit()"></b-button>
+        <EditorButton :element="element" />
         <b-button class='is-text' icon-left="close"></b-button>
         <b-button class='is-text' icon-left="menu-down"></b-button>
       </div>
     </header>
     <div class="card-content">{{ element.description }}</div>
-    <!-- EditorModal -->
-    <b-modal trap-focus has-modal-card :active.sync="modalActive">
-      <component :is="editor" :subject="element"/>
-    </b-modal>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import {
-  Element,
-  Adventure,
-  Narrative,
-  Quest,
-  Encounter,
-  PlotPoint,
-  Setting,
-  Character,
-  Area,
-  Item,
-  Faction
-} from "@/types";
-import {
-  AdventureEditor,
-  AreaEditor,
-  CharacterEditor,
-  EncounterEditor,
-  FactionEditor,
-  ItemEditor,
-  NarrativeEditor,
-  PlotPointEditor,
-  QuestEditor, 
-  SettingEditor
-} from "@/components/editors";
+import { Element } from "@/types";
+import EditorButton from "@/components/EditorButton.vue";
 
 @Component({
-  components: {
-    AdventureEditor,
-    AreaEditor,
-    CharacterEditor,
-    EncounterEditor,
-    FactionEditor,
-    ItemEditor,
-    NarrativeEditor,
-    PlotPointEditor,
-    QuestEditor, 
-    SettingEditor
-  }
+  components: { EditorButton }
 })
 export default class ElementCard extends Vue {
   @Prop()
-  element!: Element;
-  modalActive: boolean = false;
-  editor: string = "";
-
-  mounted() {
-    if (this.element instanceof Adventure) {
-      this.editor = `AdventureEditor`;
-    }
-    else if (this.element instanceof Narrative) {
-      this.editor = `NarrativeEditor`;
-    }
-    else if (this.element instanceof Quest) {
-      this.editor = `QuestEditor`;
-    }
-    else if (this.element instanceof Encounter) {
-      this.editor = `EncounterEditor`;
-    }
-    else if (this.element instanceof PlotPoint) {
-      this.editor = `PlotPointEditor`;
-    }
-    else if (this.element instanceof Setting) {
-      this.editor = `SettingEditor`;
-    }
-    else if (this.element instanceof Area) {
-      this.editor = `AreaEditor`;
-    }
-    else if (this.element instanceof Character) {
-      this.editor = `CharacterEditor`;
-    }
-    else if (this.element instanceof Item) {
-      this.editor = `ItemEditor`;
-    }
-    else if (this.element instanceof Faction) {
-      this.editor = `FactionEditor`;
-    }
-  }
-
-  edit() {
-    this.modalActive = true;
-  }
+  element!: Element
 }
 
 </script>
