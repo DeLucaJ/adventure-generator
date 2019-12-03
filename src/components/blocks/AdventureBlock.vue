aa321666e96611c35f5935cfa25224dafc5c9fe1
Joseph DeLuca <deluca@pop-os.localdomain>
2019-12-02 22:12:09 -0800

    Adds templating for WorldBlock & AdventureBlock

---
+++ src/components/blocks/AdventureBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Adventure } from "@/types";
+
+@Component({
+  name: "adventure-block"
+})
+export default class AdventureBlock extends Vue {
+  @Prop()
+  adventure!: Adventure;
+}
+</script>
---
+++ src/components/blocks/WorldBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { World } from "@/types";
+
+@Component({
+  name: "world-block"
+})
+export default class WorldBlock extends Vue {
+  @Prop()
+  World!: World;
+}
+</script>
